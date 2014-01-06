c9096b1d6e19308018d91d9b49931b6c9270fbb2
Cole Mickens <cole.mickens@gmail.com>
2014-01-05 18:30:52 -0800

    more cleanup, test passes, feedback time?

--- lib.rs
+++ lib.rs
@@ -26,25 +26,23 @@ pub static SIZE_IP_HEADER_MAX: uint = 80;
 pub static SIZE_TCP_HEADER: uint = 1;
 pub static SIZE_UDP_HEADER: uint = 8;
 
-pub enum PcapNextExError {
-    BadState,
-    ReadError,
-    Timeout,
-    EndOfCaptureFile,
+pub enum PcapError {
+    NextEx_BadState,
+    NextEx_ReadError,
+    NextEx_Timeout,
+    NextEx_EndOfCaptureFile,
+    Filter_DeviceClosed,
+    Filter_CompileError,
+    Filter_SetError,
     Unknown
-}
-
-pub enum PcapFilterError {
-    DeviceClosed, // FIX: this is a dup of the above error. Consolidate these enums?
-    CompileError,
-    SetError
 }
 
 pub struct EthernetHeader {
     DstMac:     ~[u8],
     SrcMac:     ~[u8],
-    Ethertype:       Ethertype,
+    Ethertype:  Ethertype,
 }
+impl EthernetHeader { pub fn len(&self) -> uint { 14 } }
 
 pub enum Ethertype {
     Ethertype_IP,
@@ -53,16 +51,10 @@ pub enum Ethertype {
     Ethertype_Unknown,
 }
 
-pub enum IpHeader {
-    Ipv4Header,
-    Ipv6Header,
-}
-
 pub struct Ipv4Header {
     Version:      uint,
     Ihl:          uint,
-    DiffServices: uint,
-    HeaderLength: uint,
+    DiffServices: u8,
     TotalLength:  uint,
     Id:           uint,
     Flags:        uint,
@@ -73,6 +65,7 @@ pub struct Ipv4Header {
     SrcIp:        ip::IpAddr,
     DstIp:        ip::IpAddr,
 }
+impl Ipv4Header { pub fn len(&self) -> uint { self.Ihl*4 } }
 
 pub struct Ipv6Header {
     Temp:        uint,
@@ -95,6 +88,7 @@ pub struct TcpHeader {
     Checksum:    uint,
     Urgent:      uint,
 }
+impl TcpHeader { pub fn len(&self) -> uint { 20 } }
 
 pub struct UdpHeader {
     SrcPort:     ip::Port,
@@ -102,64 +96,59 @@ pub struct UdpHeader {
     Length:      uint,
     Checksum:    uint,
 }
+impl UdpHeader { pub fn len(&self) -> uint { 8 } }
 
 pub enum DecodedPacket<'r> {
     InvalidPacket,
     EthernetPacket(EthernetHeader, &'r [u8]),
-    IpPacket(EthernetHeader, IpHeader, &'r [u8]),
+    Ipv4Packet(EthernetHeader, Ipv4Header, &'r [u8]),
     TcpPacket(EthernetHeader, Ipv4Header, TcpHeader, &'r [u8]),
     UdpPacket(EthernetHeader, Ipv4Header, UdpHeader, &'r [u8]),
 }
 
-pub fn decode_ethernet_header(h: &[u8]) -> Option<(EthernetHeader, uint)> {
-    // TODO: Check size
-
+pub fn decode_ethernet_header(h: &[u8]) -> Option<EthernetHeader> {
+    if h.len() < 14 {
+        return None
+    }
+    
     let dst_mac = h.slice(0, 6).to_owned();
     let src_mac = h.slice(6, 12).to_owned();
     let ethertype: u16 = h[12] as u16 << 8 | h[13] as u16;
-    println!("{:?}", ethertype);
     let ethertype = match ethertype {
-        // for lower ranges, it maps to a size, ugh. something EthernetHeader_Size<uint> ?
-        // TODO: use this, leverage matches,
-        // TODO: PRI: attribution http://homepage.smc.edu/morgan_david/linux/n-protocol-09-ethernet.pdf
-
         0x0800 => { Ethertype_IP }
         0x0806 => { Ethertype_ARP }
         0x8137 => { Ethertype_ARP }
         0x8100 => { Ethertype_VLAN }
         _ => { Ethertype_Unknown }
     };
-    let ether_hdr = EthernetHeader{
+
+    Some(EthernetHeader{
         DstMac: dst_mac,
         SrcMac: src_mac,
         Ethertype: ethertype,
-    };
-    Some((ether_hdr, 14))
+    })
 }
 
-pub fn decode_ip_header(h: &[u8]) -> Option<(IpHeader, uint)> {
-    decode_ipv4_header(h)
-}
-
-pub fn decode_ipv4_header(h: &[u8]) -> Option<(Ipv4Header, uint)> {
-    // TODO: Check size
-    let byte1 = h[0];
+pub fn decode_ipv4_header(h: &[u8]) -> Option<Ipv4Header> {
+    if h.len() < 20 {
+        return None
+    }
+    
     let byte2 = h[1];
 
-    let version = 4;
-    let ihl = (byte1 & 0b00001111) as uint;
+    let version = ((h[0] & 0b11110000) >> 4) as uint;
+    let ihl = (h[0] & 0b00001111) as uint;
 
-    let dscp = byte2 >> 2;
-    let ecn = byte2 & 0b00000011;
+    let dscp = h[1] >> 2;
+    let ecn = h[1] & 0b00000011;
 
     let total_len: u16 = h[2] as u16 << 8 | h[3] as u16;
     let id: u16 = h[4] as u16 << 8 | h[5] as u16;
 
-    let flags = h[6] >> 5 & 0b00000111;
-    //let frag_offset = 
+    let flags = (h[6] >> 5) & 0b00000111;
+
     let frag_offset: u16 = h[6] as u16 << 8 | h[7] as u16;
-    let frag_offset = 0b0001111111111111;
-    let frag_offset = 0;
+    let frag_offset = frag_offset & 0b0001111111111111;
 
     let ttl = h[8];
     let proto = h[9];
@@ -171,18 +160,17 @@ pub fn decode_ipv4_header(h: &[u8]) -> Option<(Ipv4Header, uint)> {
 
 
     if ihl > 15 {
-            fail!("too many ihls");
+        return None
     }
 
     if ihl > 5 {
         let options = h.slice(24, 4*(ihl-5));
     }
 
-    let res = Ipv4Header{
+    Some(Ipv4Header{
         Version:      version as uint,
         Ihl:          ihl as uint,
-        DiffServices: 0x0000, // WTF IS THIS EVEN?
-        HeaderLength: ihl * 4 as uint,
+        DiffServices: dscp,
         TotalLength:  total_len as uint,
         Id:           id as uint,
         Flags:        flags as uint,
@@ -192,26 +180,19 @@ pub fn decode_ipv4_header(h: &[u8]) -> Option<(Ipv4Header, uint)> {
         Checksum:     checksum as uint,
         SrcIp:        src_ip,
         DstIp:        dst_ip,
-        // copy the remaining payload pointer here?
-        // is that valuable?
-        // honestly, might be nicer, in case you match a TCP Packet, but want the
-        // Ethernet header and the rest of the raw bytes
-        // ... otherwise, there really wouldn't be a way to access that easily through my API...
-    };
-
-    Some((res, ihl*4))
+    })
 }
 
-pub fn decode_ipv6_header(h: &[u8]) -> Option<(Ipv6Header, uint)> {
+pub fn decode_ipv6_header(h: &[u8]) -> Option<Ipv6Header> {
     None
 }
 
-pub fn decode_tcp_header(h: &[u8]) -> Option<(TcpHeader, uint)> {
-    // TODO: Check length
+pub fn decode_tcp_header(h: &[u8]) -> Option<TcpHeader> {
+    if h.len() < 20 {
+        return None
+    }
 
-    println!("decode tcp header");
-
-    let res = TcpHeader{
+    Some(TcpHeader{
         SrcPort:     80,
         DstPort:     80,
         Seq:         80,
@@ -221,14 +202,10 @@ pub fn decode_tcp_header(h: &[u8]) -> Option<(TcpHeader, uint)> {
         Window:      80,
         Checksum:    80,
         Urgent:      80,
-    };
-
-    // TODO: Check checksum
-
-    Some((res, 20))
+    })
 }
 
-pub fn decode_udp_header(h: &[u8]) -> Option<(UdpHeader, uint)> {
+pub fn decode_udp_header(h: &[u8]) -> Option<UdpHeader> {
     if h.len() < 8 {
         return None
     }
@@ -237,20 +214,15 @@ pub fn decode_udp_header(h: &[u8]) -> Option<(UdpHeader, uint)> {
     let length: u16   = h[4] as u16 << 8 | h[5] as u16;
     let checksum: u16 = h[6] as u16 << 8 | h[7] as u16;
 
-    let res = UdpHeader{
+    Some(UdpHeader{
         SrcPort:  src_port as ip::Port,
         DstPort:  dst_port as ip::Port,
         Length:   length   as uint,
         Checksum: checksum as uint,
-
-        //Payload: h.slice_from(8),
-    };
-
-    Some((res, 8))
+    })
 }
 
 pub fn prettystr(p: &[u8]) -> ~str {
-    // HELP/TODO: probably faster to allocate al bytes since we know the size, then copy byte wise if it matches, else write 46
     let p = p.map(|&e| if e > 31 && e < 127 { e } else { '.' as u8 });
     str::from_utf8_owned(p)
 }
@@ -259,25 +231,23 @@ pub fn DecodePacket<'r>(payload: &'r [u8]) -> DecodedPacket<'r> {
     let mut payload = payload;
 
     match decode_ethernet_header(payload) {
-        Some((ether_hdr, ether_hdr_len)) => {
-            payload = payload.slice_from(ether_hdr_len);
+        Some(ether_hdr) => {
+            payload = payload.slice_from(ether_hdr.len());
             match ether_hdr.Ethertype {
-                Ethertype_IP => match decode_ip_header(payload) {
-                    Some((ip_hdr, ip_hdr_len)) => {
-                        payload = payload.slice_from(ip_hdr_len);
+                Ethertype_IP => match decode_ipv4_header(payload) {
+                    Some(ip_hdr) => {
+                        payload = payload.slice_from(ip_hdr.len());
                         match ip_hdr.Protocol {
                             TCP => match decode_tcp_header(payload) {
-                                Some((tcp_hdr, tcp_hdr_len)) => {
-                                    // tcp_hdr -> tcp_hdr.len()
-                                    // return payload with it. attempt decode, etc
-                                    payload = payload.slice_from(tcp_hdr_len);
+                                Some(tcp_hdr) => {
+                                    payload = payload.slice_from(tcp_hdr.len());
                                     TcpPacket(ether_hdr, ip_hdr, tcp_hdr, payload)
                                 }
                                 None => { InvalidPacket }
                             },
                             UserDatagram => match decode_udp_header(payload) {
-                                Some((udp_hdr, udp_hdr_len)) => {
-                                    payload = payload.slice_from(udp_hdr_len);
+                                Some(udp_hdr) => {
+                                    payload = payload.slice_from(udp_hdr.len());
                                     UdpPacket(ether_hdr, ip_hdr, udp_hdr, payload)
                                 }
                                 None => { InvalidPacket } // could let it fall through? does it matter?
@@ -295,8 +265,6 @@ pub fn DecodePacket<'r>(payload: &'r [u8]) -> DecodedPacket<'r> {
         }
     }
 }
-
-// pcap wrapper
 
 pub struct PcapDevice {
     pcap_dev: *mut pcap_t,
@@ -334,10 +302,10 @@ pub fn PcapOpenOffline(filename: &str) -> Option<PcapDevice> {
 }
 
 impl PcapDevice {
-    pub fn SetFilter(&self, dev: &str, filter_str: &str) -> Result<(), PcapFilterError> {
+    pub fn SetFilter(&self, dev: &str, filter_str: &str) -> Result<(), PcapError> {
         unsafe {
             if self.closed {
-                return Err(DeviceClosed)
+                return Err(Filter_DeviceClosed)
             }
             let mut errbuf: ~[c_schar] = vec::with_capacity(256);
             let mut netp: c_uint = 0;
@@ -350,11 +318,11 @@ impl PcapDevice {
             pcap_lookupnet(c_dev, &mut netp, &mut maskp, errbuf.as_mut_ptr());
             let res = pcap_compile(self.pcap_dev, &mut filter_program, c_filter_str, 0, netp);
             if res != 0 {
-                Err(CompileError)
+                Err(Filter_CompileError)
             } else {
                 let res = pcap_setfilter(self.pcap_dev, &mut filter_program);
                 if res != 0 {
-                    Err(SetError) // how to set the errorbuf msg in the SetError somehow?
+                    Err(Filter_SetError) // how to set the errorbuf msg in the SetError somehow?
                 } else {
                     Ok(())
                 }
@@ -362,9 +330,9 @@ impl PcapDevice {
         }
     }
 
-    pub fn NextPacketEx(&self) -> Result<PcapPacket, PcapNextExError> {
+    pub fn NextPacketEx(&self) -> Result<PcapPacket, PcapError> {
         if self.closed {
-            Err(BadState)
+            Err(NextEx_BadState)
         } else {
             unsafe {
                 let mut pkthdr_ptr: *mut Struct_pcap_pkthdr = std::unstable::intrinsics::uninit();
@@ -374,9 +342,9 @@ impl PcapDevice {
 
                 let pkt_len: uint = (*pkthdr_ptr).len as uint;
                 match result {
-                    -2 => { Err(EndOfCaptureFile) }
-                    -1 => { Err(ReadError) } // call pcap_geterr() or pcap_perror() (ret Result instead)
-                    0 => { Err(Timeout) }
+                    -2 => { Err(NextEx_EndOfCaptureFile) }
+                    -1 => { Err(NextEx_ReadError) } // call pcap_getErr(NextEx_) or pcap_perror() (ret Result instead)
+                    0 => { Err(NextEx_Timeout) }
                     1 => {
                         if pkt_len == 0 {
                             println!("ignoring zero length packet"); 
@@ -400,6 +368,7 @@ impl PcapDevice {
     }
 
     pub fn Inject(&self, pkt: DecodedPacket) -> Result<(), ()> {
+        // TODO: Implement
         unsafe {
             match pkt {
                 TcpPacket(ehdr, ihdr, thdr, pkt) => {
--- test.rs
+++ test.rs
@@ -7,11 +7,6 @@ use std::io::net::ip::Ipv4Addr;
 use pcapfe::DecodePacket;
 use pcapfe::UdpPacket;
 
-use pcapfe::Ethertype;
-use pcapfe::Ethertype::*;
-
-use pcapfe::InternetProtocolNumbers;
-
 #[test]
 fn test_decode_udp_packet() {
     let dns_pkt: &[u8] = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x30, 0x85, 0xa9, 0x40, 0x09, 0x35, 0x08, 0x00, 0x45, 0x00, 0x00, 0x31, 0x27, 0x33, 0x40, 0x00, 0x40, 0x11, 0x8f, 0x37, 0xc0, 0xa8, 0x01, 0x02, 0xc0, 0xa8, 0x01, 0xff, 0xbc, 0xad, 0x7e, 0x9c, 0x00, 0x1d, 0xc9, 0xd9, 0x4d, 0x2d, 0x53, 0x45, 0x41, 0x52, 0x43, 0x48, 0x20, 0x2a, 0x20, 0x48, 0x54, 0x54, 0x50, 0x2f, 0x31, 0x2e, 0x31, 0x0d, 0x0a];
@@ -28,38 +23,20 @@ fn test_decode_udp_packet() {
     
     match pkt {
         UdpPacket(eth_hdr, ip_hdr,  udp_hdr,  payload) => {
-            println!("{:?}", eth_hdr);
-            println!("{:?}", ip_hdr);
-            println!("{:?}", udp_hdr);
-            println!("{:?}", payload);
-
             let dst_mac = ~[0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
             let src_mac = ~[0x30, 0x85, 0xa9, 0x40, 0x09, 0x35];
             assert_eq!(eth_hdr.DstMac, dst_mac);
             assert_eq!(eth_hdr.SrcMac, src_mac);
-            match eth_hdr.Ethertype {
-                EthernetType_IP => { /* good */ }
-                //_ => { fail!("wrong kind of ethernet packet"); } // same as below?
-            }
+            // HELP: assert_is_of_type(eth_hdr.Ethertype, Ethertype_IP);
 
             assert_eq!(ip_hdr.Version, 4);
-            assert_eq!(ip_hdr.HeaderLength, 20);
             assert_eq!(ip_hdr.DiffServices, 0x00);
             assert_eq!(ip_hdr.TotalLength, 49);
             assert_eq!(ip_hdr.Id, 0x2733);
             assert_eq!(ip_hdr.Flags, 0x02);            
             assert_eq!(ip_hdr.FragOffset, 0);
             assert_eq!(ip_hdr.Ttl, 64);
-
-            match ip_hdr.Protocol {
-                UserDatagram => {
-                    // good
-                },
-                /*
-                _ => { fail("n"); }
-                // HELP: is that ^nreachable because of UdpPacket match...
-                */
-            }
+            // HELP: assert_is_of_type(ip_hdr.Protocol, UserDatagram);
             assert_eq!(ip_hdr.Checksum, 0x8f37);
             assert_eq!(ip_hdr.SrcIp, Ipv4Addr(192, 168, 1, 2));
             assert_eq!(ip_hdr.DstIp, Ipv4Addr(192, 168, 1, 255));
@@ -71,8 +48,6 @@ fn test_decode_udp_packet() {
             
             assert_eq!(payload.len(), 21);
             assert_eq!(payload, expected_payload);
-
-            println!("{}", pcapfe::prettystr(payload) );
         }, 
         g => { println!("{:?}", g); fail!("wrong packet type to start out with"); }
     }
